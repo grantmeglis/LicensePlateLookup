2254a56ab474fd7a494463d9f95e8dc8908be4f0
Meglis-Grant <meglis_grant@bah.com>
2018-04-04 23:49:15 -0400

    Virginia Page
    Virginia Page

---
+++ PageFramework/Pages/VirginiaPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PageFramework.Pages
+{
+    public class VirginiaPage
+    {
+        private int PAGE_LOAD_TIMEOUT = 7;
+
+        public void Goto()
+        {
+            Browser.Goto("https://www.dmv.virginia.gov/dmvnet/plate_purchase/select_plate.asp?PLT=&PLTNO=");
+        }
+
+        public bool IsAt()
+        {
+            return Browser.Title.Contains("License Plate Purchase");
+        }
+    }
+}
